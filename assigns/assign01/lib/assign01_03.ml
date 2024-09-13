1e688c56e3e594ef65f7d7d80fd52111dc751f52
Sarah Yuhan <sarahyuhan@crc-dot1x-nat-10-239-12-10.bu.edu>
2024-09-13 15:50:29 -0400

    Fixed assign01_02

--- assigns/assign01/lib/assign01_03.ml
+++ assigns/assign01/lib/assign01_03.ml
@@ -1,4 +1,24 @@
-open Assign01_02
+let is_prime n =
+  let rec go i =
+    if i * i > n then
+      true
+    else if n mod i = 0 then
+      false
+    else
+      go (i + 1)
+  in
+  if n < 2 then false
+  else go 2
+;;
+
+let nth_prime n =
+  let rec nth_find times number =
+    if times = n && is_prime number then number
+    else if is_prime number then nth_find (times+1) (number+1)
+    else nth_find times (number+1)
+  in
+  nth_find 0 2
+;;
 
 let nth s i =
   let prime = nth_prime i in
