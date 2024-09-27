b6d5150f0e418eed8f97b7c2c753b8ac16dc80eb
Sarah Yuhan <sarahyuhan@Sarahs-MacBook-Air.local>
2024-09-26 20:58:07 -0400

    fixed 02, 03

--- assigns/assign03/lib/assign03_02.ml
+++ assigns/assign03/lib/assign03_02.ml
@@ -1,41 +1,24 @@
-let rec nth lst n =
-  match lst, n with
-  | [], _ -> failwith "out of bounds"
-  | h::_, 0 -> h
-  | _::t, n -> nth t (n-1)
+let gen_fib l k =
+  let len = List.length l in
 
-let rec take lst n =
-  match lst, n with
-  | [], _ -> []
-  | _, 0 -> []
-  | x :: xs, n -> x :: take xs (n - 1)
-
-let rec sum lst =
-  match lst with
-  |[]-> 0
-  |h::t -> h + sum t
-
-let rec reverse lst =
-  match lst with
-  | [] -> []
-  | h :: t -> (reverse t) @ [h]
-
-let gen_fib (l : int list) (k : int) : int =
-  let len = 
-    let rec length lst acc =
-      match lst with
-      |[]-> acc
-      |_::t -> length t (acc+1)
-    in length l 0
+  let rec sum_list lst total =
+    match lst with
+    | [] -> total
+    | h :: t -> sum_list t (total + h)
   in
- 
-  let rec calc list current =
-    if current < len then
-      nth l current
-    else
-      let previous = reverse (take list len) in
-      let next = sum previous in
-      calc (next :: list) (current - 1)
+
+  let rec helper index current =
+    match current with
+    | [] -> failwith "empty"
+    | first_elem :: _ ->
+      if index = k + 1 then
+        first_elem
+      else if index < len then
+        helper (index + 1) (List.nth l index :: current)
+      else
+        let total = sum_list (List.take len current) 0 in
+        helper (index + 1) (total :: current)
   in
-  calc (reverse l) k
+
+  helper 0 l
 ;;
