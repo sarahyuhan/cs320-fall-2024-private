fdb5cd5ba1574c15292519b71e101b81094abb62
Sarah Yuhan <sarahyuhan@Sarahs-MacBook-Air.local>
2024-11-12 22:15:47 -0500

    commit

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -23,66 +23,67 @@ let rec var_replace y x e =
   | App (e1, e2) -> App (var_replace y x e1, var_replace y x e2)
   | Fun (z, e) -> if z = x then Fun (z, e) else Fun (z, var_replace y x e)
 
-let rec eval e =
-  match e with
-  | Num n -> Ok (VNum n)
-  | True -> Ok (VBool true)
-  | False -> Ok (VBool false)
-  | Unit -> Ok VUnit
-  | Var x -> Error (UnknownVar x)
-  | Bop (op, e1, e2) -> (
-    match op with
-    | And -> (
-          match eval e1 with
-          | Ok (VBool false) -> Ok (VBool false) 
-          | Ok (VBool true) -> eval e2           
-          | Ok _ -> Error (InvalidArgs op)       
+  let rec eval e =
+    match e with
+    | Num n -> Ok (VNum n)
+    | True -> Ok (VBool true)
+    | False -> Ok (VBool false)
+    | Unit -> Ok VUnit
+    | Var x -> Error (UnknownVar x)
+    | Bop (op, e1, e2) -> (
+      match op with
+      | And -> (
+            match eval e1 with
+            | Ok (VBool false) -> Ok (VBool false) 
+            | Ok (VBool true) -> eval e2           
+            | Ok _ -> Error (InvalidArgs op)       
+            | Error e -> Error e)
+      | Or -> (
+            match eval e1 with
+            | Ok (VBool true) -> Ok (VBool true)
+            | Ok (VBool false) -> eval e2
+            | Ok _ -> Error (InvalidArgs op)
+            | Error e -> Error e)
+      | _ -> (
+        match (eval e1, eval e2) with
+        | Ok (VNum n1), Ok (VNum n2) -> (
+          match op with
+          | Add -> Ok (VNum (n1 + n2))
+          | Sub -> Ok (VNum (n1 - n2))
+          | Mul -> Ok (VNum (n1 * n2))
+          | Div -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
+          | Mod -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
+          | Lt -> Ok (VBool (n1 < n2))
+          | Lte -> Ok (VBool (n1 <= n2))
+          | Gt -> Ok (VBool (n1 > n2))
+          | Gte -> Ok (VBool (n1 >= n2))
+          | Eq -> Ok (VBool (n1 = n2))
+          | Neq -> Ok (VBool (n1 <> n2))
           | _ -> Error (InvalidArgs op))
-    | Or -> (
-          match eval e1 with
-          | Ok (VBool true) -> Ok (VBool true)
-          | Ok (VBool false) -> eval e2
-          | Ok _ -> Error (InvalidArgs op)
-          | _ -> Error (InvalidArgs op))
-    | _ -> (
-      match (eval e1, eval e2) with
-      | Ok (VNum n1), Ok (VNum n2) -> (
-        match op with
-        | Add -> Ok (VNum (n1 + n2))
-        | Sub -> Ok (VNum (n1 - n2))
-        | Mul -> Ok (VNum (n1 * n2))
-        | Div -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
-        | Mod -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
-        | Lt -> Ok (VBool (n1 < n2))
-        | Lte -> Ok (VBool (n1 <= n2))
-        | Gt -> Ok (VBool (n1 > n2))
-        | Gte -> Ok (VBool (n1 >= n2))
-        | Eq -> Ok (VBool (n1 = n2))
-        | Neq -> Ok (VBool (n1 <> n2))
-        | _ -> Error (InvalidArgs op))
-    | Ok _, Ok _ -> Error (InvalidArgs op)    
-    | Error e1, _ -> Error e1
-    | _, Error e2 -> Error e2))
-  | If (e1, e2, e3) -> (
-    match eval e1 with
-    | Ok (VBool true) -> eval e2
-    | Ok (VBool false) -> eval e3
-    | Ok _ -> Error InvalidIfCond
-    | _ -> Error (InvalidArgs op))
-  | Let (x, e1, e2) -> (
-    match eval e1 with
-    | Ok v -> eval (subst v x e2)
-    | _ -> Error (InvalidArgs op))
-  | Fun (x, e) -> Ok (VFun (x, e))  
-  | App (e1, e2) -> (
-    match eval e1 with
-    | Ok (VFun (x, e)) -> (
-      match eval e2 with
-      | Ok v2 -> eval (subst v2 x e)
-      | _ -> Error (InvalidArgs op))
-    | Ok _ -> Error InvalidApp  
-    | _ -> Error (InvalidArgs op))
+      | Ok _, Ok _ -> Error (InvalidArgs op)    
+      | Error e1, _ -> Error e1
+      | _, Error e2 -> Error e2))
+    | If (e1, e2, e3) -> (
+      match eval e1 with
+      | Ok (VBool true) -> eval e2
+      | Ok (VBool false) -> eval e3
+      | Ok _ -> Error InvalidIfCond
+      | Error e -> Error e)
+    | Let (x, e1, e2) -> (
+      match eval e1 with
+      | Ok v -> eval (subst v x e2)
+      | Error e -> Error e)
+    | Fun (x, e) -> Ok (VFun (x, e))  
+    | App (e1, e2) -> (
+      match eval e1 with
+      | Ok (VFun (x, e)) -> (
+        match eval e2 with
+        | Ok v2 -> eval (subst v2 x e)
+        | Error e -> Error e)
+      | Ok _ -> Error InvalidApp  
+      | Error e -> Error e)
 
+    
 let rec subst value x expr =
   match expr with
   | Num n -> Num n
