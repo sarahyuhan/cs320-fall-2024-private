b8342fa53a75ead7447fca53449a87bf9d878ef5
Sarah Yuhan <sarahyuhan@Sarahs-MacBook-Air.local>
2024-11-12 21:23:20 -0500

    commit

--- assigns/interp01/lib.ml
+++ assigns/interp01/lib.ml
@@ -8,22 +8,86 @@ let rec lookup env x =
   | [] -> Error (UnknownVar x)
   | (y, v) :: env' -> if x = y then Ok v else lookup env' x
 
-let eval_bop op v1 v2 =
-  match op, v1, v2 with
-  | Add, VNum n1, VNum n2 -> Ok (VNum (n1 + n2))
-  | Sub, VNum n1, VNum n2 -> Ok (VNum (n1 - n2))
-  | Mul, VNum n1, VNum n2 -> Ok (VNum (n1 * n2))
-  | Div, VNum n1, VNum n2 -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
-  | Mod, VNum n1, VNum n2 -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
-  | Lt, VNum n1, VNum n2 -> Ok (VBool (n1 < n2))
-  | Lte, VNum n1, VNum n2 -> Ok (VBool (n1 <= n2))
-  | Gt, VNum n1, VNum n2 -> Ok (VBool (n1 > n2))
-  | Gte, VNum n1, VNum n2 -> Ok (VBool (n1 >= n2))
-  | Eq, VNum n1, VNum n2 -> Ok (VBool (n1 = n2))
-  | Neq, VNum n1, VNum n2 -> Ok (VBool (n1 <> n2))
-  | And, VBool b1, VBool b2 -> Ok (VBool (b1 && b2))
-  | Or, VBool b1, VBool b2 -> Ok (VBool (b1 || b2))
-  | _ -> Error (InvalidArgs op)
+let expr v =
+  match v with
+  | VFun (x, e) -> Fun (x, e)
+  | VNum n -> Num n
+  | VBool true -> True
+  | VBool false -> False
+  | VUnit -> Unit
+
+let rec var_replace y x e = 
+  match e with 
+  | Num n -> Num n
+  | True -> True
+  | False -> False
+  | Unit -> Unit
+  | Var z -> if z = x then Var y else Var z
+  | Bop (op, e1, e2) -> Bop (op, var_replace y x e1, var_replace y x e2)
+  | If (e1, e2, e3) -> If (var_replace y x e1, var_replace y x e2, var_replace y x e3)
+  | Let (z, e1, e2) -> if z = x then Let (z, var_replace y x e1, e2) else Let (z, var_replace y x e1, var_replace y x e2)
+  | App (e1, e2) -> App (var_replace y x e1, var_replace y x e2)
+  | Fun (z, e) -> if z = x then Fun (z, e) else Fun (z, var_replace y x e)
+
+let rec eval e =
+  match e with
+  | Num n -> Ok (VNum n)
+  | True -> Ok (VBool true)
+  | False -> Ok (VBool false)
+  | Unit -> Ok VUnit
+  | Var x -> Error (UnknownVar x)
+  | Bop (op, e1, e2) -> (
+    match op with
+    | And -> (
+          match eval e1 with
+          | Ok (VBool false) -> Ok (VBool false) 
+          | Ok (VBool true) -> eval e2           
+          | Ok _ -> Error (InvalidArgs op)       
+          | Error e -> Error e)
+    | Or -> (
+          match eval e1 with
+          | Ok (VBool true) -> Ok (VBool true)
+          | Ok (VBool false) -> eval e2
+          | Ok _ -> Error (InvalidArgs op)
+          | _ -> Error (InvalidArgs ops))
+    | _ -> (
+      match (eval e1, eval e2) with
+      | Ok (VNum n1), Ok (VNum n2) -> (
+        match op with
+        | Add -> Ok (VNum (n1 + n2))
+        | Sub -> Ok (VNum (n1 - n2))
+        | Mul -> Ok (VNum (n1 * n2))
+        | Div -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 / n2))
+        | Mod -> if n2 = 0 then Error DivByZero else Ok (VNum (n1 mod n2))
+        | Lt -> Ok (VBool (n1 < n2))
+        | Lte -> Ok (VBool (n1 <= n2))
+        | Gt -> Ok (VBool (n1 > n2))
+        | Gte -> Ok (VBool (n1 >= n2))
+        | Eq -> Ok (VBool (n1 = n2))
+        | Neq -> Ok (VBool (n1 <> n2))
+        | _ -> Error (InvalidArgs op))
+    | Ok _, Ok _ -> Error (InvalidArgs op)    
+    | Error e1, _ -> Error e1
+    | _, Error e2 -> Error e2))
+  | If (e1, e2, e3) -> (
+    match eval e1 with
+    | Ok (VBool true) -> eval e2
+    | Ok (VBool false) -> eval e3
+    | Ok _ -> Error InvalidIfCond
+    | _ -> Error (InvalidArgs ops))
+  | Let (x, e1, e2) -> (
+    match eval e1 with
+    | Ok v -> eval (subst v x e2)
+    | _ -> Error (InvalidArgs ops))
+  | Fun (x, e) -> Ok (VFun (x, e))  
+  | App (e1, e2) -> (
+    match eval e1 with
+    | Ok (VFun (x, e)) -> (
+      match eval e2 with
+      | Ok v2 -> eval (subst v2 x e)
+      | _ -> Error (InvalidArgs ops))
+    | Ok _ -> Error InvalidApp  
+    | _ -> Error (InvalidArgs ops))
 
 let rec interp_expr env = function
   | Num n -> Ok (VNum n)
